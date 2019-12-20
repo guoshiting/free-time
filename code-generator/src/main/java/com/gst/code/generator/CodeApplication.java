e43dcc8d79b81582fbdd59bca1289e3f912267fa
guoshiting <shiting006@163.com>
2019-12-20 11:50:09 +0800

    数据库密码加密

--- code-generator/src/main/java/com/gst/code/generator/CodeApplication.java
+++ code-generator/src/main/java/com/gst/code/generator/CodeApplication.java
@@ -3,6 +3,7 @@ package com.gst.code.generator;
 import com.gst.code.generator.common.BaseMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
